3bd66906b3b8f01697901fd50a13767fa942a5f8
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2023-01-07 21:25:31 -0600

    debug highscore display.

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,5 @@
-// when user first opens app, only instructions section will be visible.
-// when user pushes play game button (event listener 'click'), timer will start (60 seconds), instructions will be hidden, and first question will appear.
-// timer will display in the upper right corner.
-// questions will appear one at a time - after user inputs an answer the next question will appear.
-// after the user selects an answer:
-    // an event listener for 'click' will turn their selection a different color(green=correct. red=incorrect).
-    // a message will appear to validate (correct/incorrect) their answer.
-    // the next question will appear.
-// if the user selects the incorrect answer, X seconds will be removed from their remaining time.
-// once the timer reaches zero OR the user completes all questions, then the questions become invisible and the score section becomes visible.
-// the user can choose to save their score and initials. The user can choose to play again.
-// the high score section saves the top X highest scores and initials. 
+// after the user selects an answer a message will appear to validate (correct/incorrect) their answer.
 // the high score form will validate the submission for initials. 
-// the user can view high scores from the instructions page or the score page. Otherwise, the high score section is hidden.
 
 // these variable are the locations of where the top scores will populate.
 var firstInitialsSpan = document.querySelector("#initials1");
@@ -40,21 +28,16 @@ let highScoresSpanArray = [
     fifthScoreSpan
 ];
 
-var scoreArray = [];
+var scoreArray = []; //set the array in to local storage? JSON stringify?
+console.log(scoreArray); //test
 
 // variables for different displays. used to set displays as visible/none.
 var instructionsDisplay = document.querySelector("#instructions");
 var quizDisplay = document.querySelector("#quiz");
-var scoreDisplay = document.querySelector("#scores");
+var scoreDisplay = document.querySelector("#highScores");
 var scoreForm = document.querySelector("#scoreForm");
 
 function init() {
-    var storedScores = JSON.parse(localStorage.getItem("userScoreAndInitials"));
-
-    if (storedScores !== null) {
-        scoreArray.push(storedScores);
-    }
-
     renderHighScores();
 
     instructionsDisplay.style.display = "default";
@@ -64,6 +47,11 @@ function init() {
 }
 
 function renderHighScores() {
+    var storedScores = JSON.parse(localStorage.getItem("userScoreAndInitials"));
+    if (storedScores !== null) {
+        scoreArray.push(storedScores);
+    }
+
     for (var i = 0; i < scoreArray.length; i++) {
 
         // scoreArray.sort.score(function(a,b){return b.score - a.score});
@@ -90,6 +78,11 @@ submitButton.addEventListener("click", function(event) {
     localStorage.setItem("userScoreAndInitials", JSON.stringify(userScoreAndInitials));
 
     renderHighScores();
+
+    instructionsDisplay.style.display = "none";
+    scoreDisplay.style.display = "flex";
+    quizDisplay.style.display = "none";
+    scoreForm.style.display = "none";
 })
 
 // Variables for taking the quiz.
@@ -111,13 +104,12 @@ var timerSpan = document.querySelector("#timer");
 var userScoreSpan = document.querySelector("#userScoreSpan");
 var lastQuestion = document.querySelector("#q20");
 var timerCard = document.querySelector("#timerCard")
+var secondsLeft = 10;
 
 function initTimer() {
-    var secondsLeft = 60;
-    
     var clickLastQ = false;
-    lastQuestion.addEventListener("click", function() {clickLastQ = true;})
     clickIncorrect = false;
+    lastQuestion.addEventListener("click", function() {clickLastQ = true;})
 
     var timerInterval = setInterval(function() {
         timerSpan.textContent = secondsLeft;
@@ -130,8 +122,7 @@ function initTimer() {
         })
 
         if (secondsLeft > 0 && clickIncorrect === true) {
-            // timerCard.style.backgroundColor = "red"; //TODO: blink red for 2 seconds.
-            secondsLeft = secondsLeft - 3;
+            incorrectAnimation();
         } else if (secondsLeft === 0 || secondsLeft < 0) {
             userScoreSpan.textContent = secondsLeft;
             clearInterval(timerInterval);
@@ -144,6 +135,12 @@ function initTimer() {
 
         clickIncorrect = false;
     }, 1000);
+}
+
+function incorrectAnimation() {
+    secondsLeft = secondsLeft - 3;
+    console.log('incorrect'); //test
+    // timerCard.style.backgroundColor = "red"; //TODO: blink red for 2 seconds.
 }
 
 function gameOver() {
